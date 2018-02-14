ee3f508510feb8ec395fab68067e350326c34060
Wyn Price <wynprice999@gmail.com>
2018-02-14 20:56:50 +0000

    Updated versions, and added lang files

--- src/main/java/com/wynprice/cloak/CloakMod.java
+++ src/main/java/com/wynprice/cloak/CloakMod.java
@@ -20,7 +20,7 @@ public class CloakMod
 {
     public static final String MODID = "cloak";
     public static final String NAME = "Cloak";
-    public static final String VERSION = "0.0.5";
+    public static final String VERSION = "0.1.0";
 
     public static final Logger LOGGER = LogManager.getLogger(MODID);
 
